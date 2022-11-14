b7440fff7eee747dedfbacd845e68e0f42bac0b9
Rajat Rana <rajat.rana@vidyamantra.com>
2022-11-14 10:09:05 +0530

    added functionality of currency conversion

--- app.vue
+++ app.vue
@@ -1,5 +1,81 @@
 <template>
-  <div>
-    <h1>fuck you</h1>
+  <div class="">
+
+    <div class="container1">
+      <select class="border border-black" name="currency" id="currencyList" v-model="selectedCurrency1">
+        
+        <option v-for="name in computedCurrencyList1" :key="name" >{{ name }}</option>
+      </select>
+
+      <input class="block border-black" type="number" v-model="val1" placeholder="Enter Value"> 
+    </div>
+
+    <div class="container2">
+      <select class="border border-black" name="currency" id="currencyList" v-model="selectedCurrency2">
+        
+        <option v-for="name in computedCurrencyList2" :key="name" >{{ name }}</option>
+      </select>
+      <input class="block border-blue-50" type="number" v-model="val2" placeholder="Enter Value">
+       
+    </div>
+  
   </div>
+
 </template>
+
+<script setup lang="ts">
+import type {currencies} from "./types/CurrencyListType"
+
+
+// currencies and their value equivalent to 1 US Dollar
+const currencyWithVal = {
+  "Kuwaiti Dinar": 0.31,
+  "Bahraini Dinar": 0.38,
+  "Omani Rial": 0.39,
+  "British Pound Sterling": 0.85,
+  "European Euro": 0.98,
+  "US Dollar": 1,
+  "Brunei Dollar": 1.37,
+  "New Zealand Dollar": 1.66,
+  "Aruban Florin": 1.80,
+  "Indian Rupee": 80.80
+}
+
+// arr of all currencies
+const currencyNames = ref(["Kuwaiti Dinar", "Bahraini Dinar", "Omani Rial", "British Pound Sterling", "European Euro", "US Dollar", "Brunei Dollar", "New Zealand Dollar", "Aruban Florin", "Indian Rupee"])
+
+// two selected currencies
+let selectedCurrency1 = ref<currencies>("Kuwaiti Dinar")
+let selectedCurrency2 = ref<currencies>("Indian Rupee")
+
+// values of two selected currencies
+let val1 = ref<number>()
+let val2 = ref<number>()
+
+// this removes selectedCurrency2 from computedCurrencyList1 
+const computedCurrencyList1 = computed(() => {
+  return currencyNames.value.filter((currency) => currency !== selectedCurrency2.value)
+})
+
+// this removes selectedCurrency1 from computedCurrencyList2
+const computedCurrencyList2 = computed(() => {
+  return currencyNames.value.filter((currency) => currency !== selectedCurrency1.value)
+})
+
+
+// watcher for val1 and val2 if any of these value change the other value get updated
+watch([val1, val2], ([newVal1, newVal2], [oldVal1, oldVal2]): void=>{
+  if (newVal1 !== oldVal1){
+    // logic for converting currecy
+    val2.value = (newVal1! / currencyWithVal[selectedCurrency1.value]) * currencyWithVal[selectedCurrency2.value]
+  }
+  else if (newVal2 !== oldVal2){
+    val1.value = ((newVal2! / currencyWithVal[selectedCurrency2.value]) * currencyWithVal[selectedCurrency1.value])
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ types/CurrencyListType.ts
@@ -0,0 +1,3 @@
+type currencies = "Kuwaiti Dinar" | "Bahraini Dinar" | "Omani Rial" | "British Pound Sterling" | "European Euro" | "US Dollar" | "Brunei Dollar" | "New Zealand Dollar" | "Aruban Florin" | "Indian Rupee"
+
+export type {currencies}
